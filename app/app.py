b805d897a29dcf2ac716335e9317af307db84552
Naseef <naseef@example.com>
2026-06-05 09:33:24 +0000

    add monitoring stack with Prometheus and Grafana

---
+++ app/app.py
@@ -0,0 +1,19 @@
+
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_requests_total', 'Total request count')
+
+@app.route("/")
+def home():
+    REQUEST_COUNT.inc()
+    return "<h1>Monitoring Stack is working!</h1>"
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
